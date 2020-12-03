a09dc6a31d0683a4ec7e785e0136b6a2ef7972b3
Anibal Ventura <anibalventura20@gmail.com>
2020-12-03 10:49:21 -0400

    Add location provider and permissions.

--- app/src/main/java/com/anibalventura/todayweather/ui/HomeFragment.kt
+++ app/src/main/java/com/anibalventura/todayweather/ui/HomeFragment.kt
@@ -1,24 +1,122 @@
 package com.anibalventura.todayweather.ui
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.location.Location
+import android.location.LocationManager
+import android.net.Uri
 import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.afollestad.materialdialogs.MaterialDialog
+import com.anibalventura.todayweather.R
 import com.anibalventura.todayweather.databinding.FragmentHomeBinding
+import com.google.android.gms.location.*
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var latitude: Double = 0.0
+    private var longitude: Double = 0.0
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         binding.lifecycleOwner = this
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+
+        getLocation()
 
         return binding.root
+    }
+
+    /** ===================================== Permissions. ===================================== **/
+
+    private fun getLocation() {
+        Dexter.withContext(requireContext()).withPermissions(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ).withListener(object : MultiplePermissionsListener {
+
+            override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                if (isLocationEnabled() && report!!.areAllPermissionsGranted()) requestLocationData()
+            }
+
+            override fun onPermissionRationaleShouldBeShown(
+                permissions: MutableList<PermissionRequest>, token: PermissionToken
+            ) = permissionDeniedDialog()
+
+        }).onSameThread().check()
+    }
+
+    private fun permissionDeniedDialog() {
+        MaterialDialog(requireContext()).show {
+            title(R.string.dialog_permission_denied)
+            message(R.string.dialog_permission_denied_msg)
+            negativeButton(R.string.dialog_cancel)
+            positiveButton(R.string.dialog_go_to_settings) {
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    intent.data = Uri.fromParts("package", activity?.packageName, null)
+                    startActivity(intent)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+
+    /** ===================================== Get Location. ===================================== **/
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager =
+            activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val locationEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+
+        if (!locationEnabled) {
+            Toast.makeText(
+                requireContext(), "Your GPS is OFF. Please turn it ON.", Toast.LENGTH_SHORT
+            ).show()
+            startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+        }
+
+        return locationEnabled
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestLocationData() {
+        val locationRequest = LocationRequest()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = 2000
+        locationRequest.numUpdates = 1
+
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest, locationCallBack(), Looper.myLooper()
+        )
+    }
+
+    private fun locationCallBack() = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult?) {
+            val lastLocation: Location = locationResult!!.lastLocation
+            latitude = lastLocation.latitude
+            longitude = lastLocation.longitude
+        }
     }
 
     /** ===================================== Fragment exit/close. ===================================== **/
--- app/src/main/java/com/anibalventura/todayweather/utils/Constants.kt
+++ app/src/main/java/com/anibalventura/todayweather/utils/Constants.kt
@@ -2,4 +2,6 @@ package com.anibalventura.todayweather.utils
 
 object Constants {
 
+    // Permissions
+    const val PLACE_REQUEST_CODE = 1
 }
